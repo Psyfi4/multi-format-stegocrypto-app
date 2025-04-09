4b696158cd4a8361575394627bed04889b96251a
Psyfi_4 <armansinhavines@gmail.com>
2025-04-10 02:04:54 +0530

    Create recover.py

---
+++ stegocrypto/recover.py
@@ -0,0 +1,11 @@
+# stegocrypto/audio_stego.py
+def hide_in_audio(audio_file, message): pass
+def extract_from_audio(audio_file): return ""
+
+# stegocrypto/pdf_stego.py
+def hide_in_pdf(pdf_file, message): pass
+def extract_from_pdf(pdf_file): return ""
+
+# stegocrypto/video_stego.py
+def hide_in_video(video_file, message): pass
+def extract_from_video(video_file): return ""
